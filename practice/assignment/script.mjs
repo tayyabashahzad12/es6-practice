db6c6e8be6b627fa14d6b6e6153f83375b745174
tayyabashahzad12 <tayyabashahzad12@gmail.com>
2024-12-11 05:35:39 -0800

    feat: added hobbies,nationality,surname

--- practice/assignment/script.mjs
+++ practice/assignment/script.mjs
@@ -1,14 +1,32 @@
 const alertFunction = () => {
-    const name = prompt("What is your name?", "Name");
+    const name = prompt("What is your name?", "Name" );
     if (name) {
-        document.write(`My name is ${name}`);
+        document.write(`My name is ${name}.<br>`);
     }
-    
+
     const age = prompt("What is your age?", "18");
     if (age < 18) {
-        return document.write(` i am ${age} years old and is a minor.`);
-    } 
-    if (age > 18) {
-        return document.write(` i am  ${age} years old and is an adult.`);
+         document.write(` i am ${age} years old and is a minor. <br>`);
     }
-    };
+    if (age > 18) {
+         document.write(` i am  ${age} years old and is an adult. <br>`);
+    }
+
+    const hobbies = prompt("What are your hobbies?","hobbies");
+
+    if (hobbies) {
+        document.write(` my hobbies are ${hobbies}.<br>`);
+    }
+
+    const nationality = prompt("what is your nationality?","nationality");
+
+    if (nationality) {
+        document.write(` my nationality is ${nationality}.<br>`)
+    }
+
+
+    const surName = prompt("what is your surName?","surName")
+    if (surName) {
+        document.write(`my surName is ${surName}.<br>`)
+    }
+};
