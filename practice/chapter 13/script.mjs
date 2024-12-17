f71cb94d5dc3a5d8fcf10bc0efb81b26be64e73a
tayyabashahzad12 <tayyabashahzad12@gmail.com>
2024-12-17 11:54:32 -0800

    feat:added chap 13 practice

---
+++ practice/chapter 13/script.mjs
@@ -0,0 +1,31 @@
+const alertFunction=()=>{
+ {
+        // const weight = prompt("Apna wazan bataye (pounds mein):");
+        // const speed = prompt("40 yards bhagnay ka waqt bataye (seconds mein):");
+    
+        // if (weight > 300 && speed < 6) {
+        //     document.write("Aapko NFL lineman tryouts ke liye bulaya jaayega!");
+        // } 
+        // else {
+        //     document.write("Aap NFL tryouts ke liye qualify nahi karte.");
+        // }
+const weight = prompt("what is your weight:");
+        const time = prompt("what is your runing time in yards:");
+    const age = prompt("what is your age:");
+        const gender = prompt("what is your gender:");
+    
+         if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+            document.write("Come to our tryout!");
+             }
+             else {
+             document.write("Come to our cookout!");
+            }
+
+// const age = prompt("what is your age")
+// const resident = prompt("what is your resident")
+// if (age > 65 && res === "U.S.") {
+//  document.write("its a pass")
+// }
+
+}
+}
